1751fd4a93fa2854fd7dbb521fdc7c60d375b5ea
thinhvuthecreator <thinhvuh@gmail.com>
2023-08-30 11:01:54 +0700

    first commit

---
+++ XenSpiritsOnlineStore/resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<html>
+    
+
+    <head>
+      <title>Xen. Spirits®</title>
+      <link rel="stylesheet" href="{{asset('CSS/style.css')}}"/>
+      <link rel="icon" href="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/372976448_775680661023410_8267997135724052810_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=B-00sEJbIewAX-41QIX&_nc_ht=scontent.fdad3-5.fna&oh=00_AfBliIN0tbSfO5U2uy8gB1WuzOtvxDrzsCvScoOvRT5iYQ&oe=64F42C72" type="image/icon type">
+    </head>    
+
+    <body>
+     
+      <div class="OnTopHomePage">
+      <img class="XenLogo" 
+           src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/371069644_775675164357293_3698107806014934575_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=DVVao_pWLnUAX97HODH&_nc_ht=scontent.fdad3-4.fna&oh=00_AfDVRF_SuOhNQcvS_pIzb4v_fH8IjvZD3pNu2IOpjyjKaA&oe=64F2CB25"
+           alt="img">
+      </img>
+      </div>
+    
+      
+      
+    </body>
+      
+  
+  
+</html>
