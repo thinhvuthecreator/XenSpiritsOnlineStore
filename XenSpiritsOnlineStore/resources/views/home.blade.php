13529aefb5e6a90a25f59aa508b3ff327b8bfc31
thinhvuthecreator <thinhvuh@gmail.com>
2023-08-31 23:33:23 +0700

    tim cach them event

--- XenSpiritsOnlineStore/resources/views/home.blade.php
+++ XenSpiritsOnlineStore/resources/views/home.blade.php
@@ -15,18 +15,30 @@
            src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/371069644_775675164357293_3698107806014934575_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=DVVao_pWLnUAX97HODH&_nc_ht=scontent.fdad3-4.fna&oh=00_AfDVRF_SuOhNQcvS_pIzb4v_fH8IjvZD3pNu2IOpjyjKaA&oe=64F2CB25"
            alt="img">
       </img>
+      
+      <ul class="navigator-header">
+        <li><a href="#">Sản phẩm</a></li>
+        <li><a href="#">Giỏ hàng</a></li>
+        <li><a href="#">Tìm kiếm</a></li>
+        <li><a href="#">Hướng dẫn kích cỡ</a></li>
+      </ul>
       </div>
 
-      <div class="mainImageHome">
-       <div class="aoVaTenSanPham">
-        <img class="mainImageHome1" src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/365406008_180389101714595_8719181203100693939_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=X-XOVk3FkEoAX-w-0fY&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfBdFwUFRUg7cnp_mnf20e5d0pImU1NWhmWSQ6CVslKxPg&oe=64F3FF58"></img>
-        <h1 class="tenSanPham"> Tên sản phẩm </h1>
-       </div>
-       <img class="mainImageHome2" src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/365417176_180389305047908_2672613284199020113_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=jlQNFBbfqosAX-OmjHk&_nc_ht=scontent.fdad3-4.fna&oh=00_AfAZEuA1yB8sdnKlH3C3BqOUhYiVYdVA4gq-2BlUZpmLag&oe=64F48D2C"></img>
-      </div>
-      <h1 class="collection">Xen. Collections</h1>
+      <header class="mainImageHome">
+        <div class="aoVaTenSanPham">
+          <img class="mainImageHome1" src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/365406008_180389101714595_8719181203100693939_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=X-XOVk3FkEoAX-w-0fY&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfBdFwUFRUg7cnp_mnf20e5d0pImU1NWhmWSQ6CVslKxPg&oe=64F3FF58"></img>
+          <h1 class="tenSanPham"> Xen. Spirits Basic Logo T-Shirt </h1>
+          <button class="right-switch-btn"><img src="https://www.nicepng.com/png/detail/245-2454141_arrow-show-symbol-direction-right-next-characters-svg.png"></img></buton>
+          <button class="left-switch-btn"><img src="https://www.svgrepo.com/download/26036/left-arrow.svg"></img></buton>
+        </div>
+        
+        <img class="mainImageHome2" src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/365417176_180389305047908_2672613284199020113_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=jlQNFBbfqosAX-OmjHk&_nc_ht=scontent.fdad3-4.fna&oh=00_AfAZEuA1yB8sdnKlH3C3BqOUhYiVYdVA4gq-2BlUZpmLag&oe=64F48D2C"></img>
+      </header>
+      <h1 class="lookbook">XEN® LOOKBOOK</h1>
       <img class="imgEx" src="https://hashtaglegend.com/storage/app/media/2018%20Articles/August%202018/BAPE%20SSFA%202018/BAPE_SS18Lookbook_1.jpg"></img> 
+       
 
+      <footer> By Xen.Spirits® | Since2023.</footer>
    
     </body>
       
