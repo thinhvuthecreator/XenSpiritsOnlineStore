30afa225406db2190556b303e3bd8a0e01e9d2b8
thinhvuthecreator <thinhvuh@gmail.com>
2023-09-01 21:24:17 +0700

    tim cach them file local vao project

--- XenSpiritsOnlineStore/resources/views/home.blade.php
+++ XenSpiritsOnlineStore/resources/views/home.blade.php
@@ -3,7 +3,7 @@
 
     <head>
       <title>Xen. Spirits®</title>
-      <link rel="stylesheet" href="{{asset('CSS/style.css')}}"/>
+      <link rel="stylesheet" href="{{asset('CSS/homestyle.css')}}"/>
       <link rel="icon" href="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/372976448_775680661023410_8267997135724052810_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=B-00sEJbIewAX-41QIX&_nc_ht=scontent.fdad3-5.fna&oh=00_AfBliIN0tbSfO5U2uy8gB1WuzOtvxDrzsCvScoOvRT5iYQ&oe=64F42C72" type="image/icon type">
     </head>    
 
@@ -12,19 +12,22 @@
       <div class="OnTopHomePage">
       <h1 class="XenTopPage">XEN.SPIRITS ®</h1>
       <img class="XenLogo" 
-           src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/371069644_775675164357293_3698107806014934575_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=DVVao_pWLnUAX97HODH&_nc_ht=scontent.fdad3-4.fna&oh=00_AfDVRF_SuOhNQcvS_pIzb4v_fH8IjvZD3pNu2IOpjyjKaA&oe=64F2CB25"
-          
+           src="https://scontent-hkg4-2.xx.fbcdn.net/v/t39.30808-6/374559179_776688794255930_1761819913023539923_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=49d041&_nc_ohc=2U4jOYB6S_kAX-b_Gwc&_nc_ht=scontent-hkg4-2.xx&oh=00_AfAjRHkFfgrDUaxykR8GPy3Cz6SIWtf5acJKO4Uh6Uf2kQ&oe=64F6E3A4"
            alt="img">
       </img>
-      
+      <img class="XenLogo" 
+           src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/371069644_775675164357293_3698107806014934575_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=DVVao_pWLnUAX97HODH&_nc_ht=scontent.fdad3-4.fna&oh=00_AfDVRF_SuOhNQcvS_pIzb4v_fH8IjvZD3pNu2IOpjyjKaA&oe=64F2CB25"
+           alt="img">
+      </img>
+   
       <ul class="navigator-header">
+        <li><a href="http://localhost:8000/">Trang chủ</a></li>
         <li><a href="http://localhost:8000/products">Sản phẩm</a></li>
         <li><a href="http://localhost:8000/wishlist">Giỏ hàng</a></li>
         <li><a href="#">Tìm kiếm</a></li>
         <li><a href="http://localhost:8000/size-guide">Hướng dẫn kích cỡ</a></li>
       </ul>
       </div>
-
       <header class="mainImageHome">
         <div class="aoVaTenSanPham">
           <img class="mainImageHome1" src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/365406008_180389101714595_8719181203100693939_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=X-XOVk3FkEoAX-w-0fY&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfBdFwUFRUg7cnp_mnf20e5d0pImU1NWhmWSQ6CVslKxPg&oe=64F3FF58"></img>
@@ -36,13 +39,22 @@
         <img class="mainImageHome2" 
              src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/365417176_180389305047908_2672613284199020113_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=jlQNFBbfqosAX-OmjHk&_nc_ht=scontent.fdad3-4.fna&oh=00_AfAZEuA1yB8sdnKlH3C3BqOUhYiVYdVA4gq-2BlUZpmLag&oe=64F48D2C">
         </img>
+
+        <ul class="social-media">
+        <li><a href="https://www.facebook.com/profile.php?id=100092305197104" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1200px-Facebook_Logo_%282019%29.png"></img></a></li>
+        <li><a href="https://www.instagram.com/xen.spirits/" target="_blank"><img src="https://i0.wp.com/soundings.com/wp-content/uploads/2021/03/INSTAGRAM-Logo-Round-1000px.png?ssl=1"></img></a></li>
+        <li><a><img src="https://static.vecteezy.com/system/resources/thumbnails/016/716/450/small/tiktok-icon-free-png.png" target="_blank"></img></a></li>
+      </ul>
       </header>
+
+  
+
       <h1 class="lookbook">XEN® LOOKBOOK</h1>
       <img class="imgEx" 
            src="https://hashtaglegend.com/storage/app/media/2018%20Articles/August%202018/BAPE%20SSFA%202018/BAPE_SS18Lookbook_1.jpg">
       </img> 
        
-
+      
       <footer>
         <li> By Xen.Spirits® | Since2023.</li>
       </footer>
--- XenSpiritsOnlineStore/resources/views/sizeGuide.blade.php
+++ XenSpiritsOnlineStore/resources/views/sizeGuide.blade.php
@@ -1,1 +1,32 @@
-<h4>HƯỚNG DẪN KÍCH CỠ</h4>
+
+<head>
+  
+<title>Xen. Spirits | Size Guide</title>
+<link rel="stylesheet" href="{{asset('CSS/sizeguidestyle.css')}}"/>
+<link rel="icon" href="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/372976448_775680661023410_8267997135724052810_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=B-00sEJbIewAX-41QIX&_nc_ht=scontent.fdad3-5.fna&oh=00_AfBliIN0tbSfO5U2uy8gB1WuzOtvxDrzsCvScoOvRT5iYQ&oe=64F42C72" type="image/icon type">
+
+</head>
+
+<body>
+
+    <div class="OnTopHomePage">
+      <h1 class="XenTopPage">XEN.SPIRITS ®</h1>
+      <img class="XenLogo" 
+           src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/371069644_775675164357293_3698107806014934575_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=DVVao_pWLnUAX97HODH&_nc_ht=scontent.fdad3-4.fna&oh=00_AfDVRF_SuOhNQcvS_pIzb4v_fH8IjvZD3pNu2IOpjyjKaA&oe=64F2CB25"
+          
+           alt="img">
+      </img>
+      
+      <ul class="navigator-header">
+        <li><a href="http://localhost:8000/">Trang chủ</a></li>
+        <li><a href="http://localhost:8000/products">Sản phẩm</a></li>
+        <li><a href="http://localhost:8000/wishlist">Giỏ hàng</a></li>
+        <li><a href="#">Tìm kiếm</a></li>
+        <li><a href="http://localhost:8000/size-guide">Hướng dẫn kích cỡ</a></li>
+      </ul>
+    </div>
+    <hr></hr>
+    <img class="size-guide-image" src="https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-6/374576173_776813860910090_7201308764698656560_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=49d041&_nc_ohc=6qaefpsbv38AX9Erg5-&_nc_ht=scontent.fdad3-1.fna&oh=00_AfASA6-uG2j-KI5MLADbGC-ZzTzk7z9xy1LLTI0Cvl7WhA&oe=64F6F2B5"></img>
+    <img class="size-guide-image-detail" src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/374255144_776826524242157_5531725653930831589_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=49d041&_nc_ohc=F1GEuFAl90YAX_oNhic&_nc_ht=scontent.fdad3-4.fna&oh=00_AfCGbgS0uvMoK8tGLsn9bXimcQsZj2QvGSU9DsK6VOBfxg&oe=64F5D4B2"></img>
+    <img class="botton-corner-logo" src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/374556091_776835857574557_6009278897638642038_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=49d041&_nc_ohc=mhR7uIDCBesAX_jqzcp&_nc_ht=scontent.fdad3-4.fna&oh=00_AfB-t9GkpSlCjwYC5uoHQnQMISKD3QWgAmYarmuk4urozA&oe=64F6A161"></img>
+</body>
