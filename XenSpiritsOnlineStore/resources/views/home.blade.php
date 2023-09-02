ebad94aec7d096ba780eb74f02da590b73b7fb45
thinhvuthecreator <thinhvuh@gmail.com>
2023-09-02 15:59:17 +0700

    da them duoc file local vao project

--- XenSpiritsOnlineStore/resources/views/home.blade.php
+++ XenSpiritsOnlineStore/resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <head>
       <title>Xen. Spirits®</title>
       <link rel="stylesheet" href="{{asset('CSS/homestyle.css')}}"/>
-      <link rel="icon" href="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/372976448_775680661023410_8267997135724052810_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=B-00sEJbIewAX-41QIX&_nc_ht=scontent.fdad3-5.fna&oh=00_AfBliIN0tbSfO5U2uy8gB1WuzOtvxDrzsCvScoOvRT5iYQ&oe=64F42C72" type="image/icon type">
+      <link rel="icon" href="{{asset('Resource/XenTitleIcon.png')}}" type="image/icon type">
     </head>    
 
     <body>
@@ -32,8 +32,8 @@
         <div class="aoVaTenSanPham">
           <img class="mainImageHome1" src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/365406008_180389101714595_8719181203100693939_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=X-XOVk3FkEoAX-w-0fY&_nc_ht=scontent.fsgn5-2.fna&oh=00_AfBdFwUFRUg7cnp_mnf20e5d0pImU1NWhmWSQ6CVslKxPg&oe=64F3FF58"></img>
           <h1 class="tenSanPham"> Xen. Spirits Basic Logo T-Shirt </h1>
-          <button class="right-switch-btn"><img src="https://www.nicepng.com/png/detail/245-2454141_arrow-show-symbol-direction-right-next-characters-svg.png"></img></buton>
-          <button class="left-switch-btn"><img src="https://www.svgrepo.com/download/26036/left-arrow.svg"></img></buton>
+          <button class="right-switch-btn"><img src="{{asset('Resource/right-arrow.png')}}"></img></buton>
+          <button class="left-switch-btn"><img src="{{asset('Resource/left-arrow.png')}}"></img></buton>
         </div>
         
         <img class="mainImageHome2" 
@@ -53,6 +53,7 @@
       <img class="imgEx" 
            src="https://hashtaglegend.com/storage/app/media/2018%20Articles/August%202018/BAPE%20SSFA%202018/BAPE_SS18Lookbook_1.jpg">
       </img> 
+
        
       
       <footer>
