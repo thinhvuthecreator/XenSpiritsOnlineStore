1063e410693c60900f4c32f211231bdc0ade7e25
thinhvuthecreator <thinhvuh@gmail.com>
2023-09-05 13:20:48 +0700

    thiet ke giao dien trang dang ky

--- XenSpiritsOnlineStore/resources/views/register.blade.php
+++ XenSpiritsOnlineStore/resources/views/register.blade.php
@@ -1,8 +1,30 @@
 <head>
       <title>Xen. Spirits® | Register</title>    
-      <link rel="stylesheet" href="{{asset('CSS/grid.css')}}">
+      <link rel="stylesheet" href="{{asset('CSS/registerstyle.css')}}">
       <link rel="icon" href="{{asset('Resource/XenTitleIcon.png')}}" type="image/icon type">
 </head>
 <body>
     @include('Reusable/on-top-page')
+
+    <div class="register-box-background">
+
+    </div>
+        <div class="register-box">
+            <form>
+            <ul>
+                <div>
+                <li><input type="text"/></li>
+                <div>
+                <div>
+                <li><input type="text"/></li>
+                <div>
+                <div>
+                <li><input type="text"/></li>
+                <div>
+                <div>
+                <li><input type="text"/></li>
+                <div>
+            </ul>
+            </form>
+        </div>
 </body>
