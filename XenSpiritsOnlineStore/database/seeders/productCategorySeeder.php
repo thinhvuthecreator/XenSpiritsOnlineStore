2e6fe631f3a910dc018d670d9220af30e6cedb2a
thinhvuthecreator <thinhvuh@gmail.com>
2023-09-15 19:20:13 +0700

    da seed duoc du lieu, bay gio tap trung xay dung trang quan tri

--- XenSpiritsOnlineStore/database/seeders/productCategorySeeder.php
+++ XenSpiritsOnlineStore/database/seeders/productCategorySeeder.php
@@ -13,6 +13,8 @@ class productCategorySeeder extends Seeder
      */
     public function run(): void
     {
-
+        productCategory::create([
+            'name'=> 'Accessories'
+        ]);
     }
 }
--- XenSpiritsOnlineStore/resources/views/Layout/Admin.blade.php
+++ XenSpiritsOnlineStore/resources/views/Layout/Admin.blade.php
@@ -12,9 +12,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="{{asset('AdminLTE/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="AdminLTE/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{asset('AdminLTE/dist/css/adminlte.min.css')}}">
   <!-- Icon -->
   <link rel="icon" href="{{asset('Resource/XenTitleIcon.png')}}" type="image/icon type">
 </head>
@@ -40,10 +40,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="{{asset('dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
