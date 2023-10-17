de07c6dfe0763d393b34737aaf7064d5f127a7bb
thinhvuthecreator <thinhvuh@gmail.com>
2023-10-17 13:22:29 +0700

    da them duoc chuc nang edit nhan vien

--- XenSpiritsOnlineStore/app/Http/Controllers/StaffController.php
+++ XenSpiritsOnlineStore/app/Http/Controllers/StaffController.php
@@ -6,7 +6,7 @@ use App\Models\Role;
 use App\Models\Staff;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
-
+use Illuminate\Support\Facades\DB;
 
 class StaffController extends Controller
 {
@@ -40,15 +40,18 @@ class StaffController extends Controller
         ];
         $request->validate($rules,$messages);
 
+        $image = $request->file('staff_image_input');
+        $storedPath = $image->move('Resource/staff_Images', $image->getClientOriginalName());
+
         Staff::create([
             'full_name' => $request->staff_name_input,
-            'image' => $request->staff_image_input,
+            'image' => $image->getClientOriginalName(),
             'phone' => $request->phone_input,
             'citizen_id' => $request->citizen_id_input,
             'date_of_birth' => $request->staff_birth_input,
         ]);
-        $image = $request->file('staff_image_input');
-        $storedPath = $image->move('Resource/staff_Images', $image->getClientOriginalName());
+
+        
         return redirect()->back();
         // echo 'citizen id : '.str::length($request->citizen_id_input)."\n";
         // echo 'phone : '.str::length($request->phone_input);
@@ -57,6 +60,50 @@ class StaffController extends Controller
     public function EditStaff($id)
     {
         $roles = Role::all();
-        return view('forAdmin.Staff.edit',compact('roles'));
+        $staff = DB::table('staff')->where('id',$id)->first();
+        return view('forAdmin.Staff.edit',compact('roles','staff'));
+    }
+
+    public function EditStaffData(Request $request)
+    {
+        $rules = [
+            'staff_name_input' => 'required',
+            'staff_image_input'=> 'image',
+            'citizen_id_input' => 'required|numeric|digits:12',
+            'phone_input' => 'required|numeric|digits:10',
+            'staff_birth_input' => 'required'
+        ];
+        $messages = [
+            'required' => 'Trường này không được để trống',
+            'image' => 'Đây không phải là file ảnh ! Vui lòng chọn file ảnh',
+            'numeric' => 'Vui lòng nhập số',
+            'phone_input.digit' => 'Số điện thoại không hợp lệ !',
+            'citizen_id_input.digit' => 'Số căn cước không hợp lệ !'
+
+        ];
+
+        $request->validate($rules,$messages);
+        $image_update ="";
+        if($request->file('staff_image_input')) // có hành động đổi ảnh
+        {
+            $image = $request->file('staff_image_input');
+            $storedPath = $image->move('Resource/staff_Images', $image->getClientOriginalName());
+            $image_update = $image->getClientOriginalName();
+        }
+        else // không có hành động đổi ảnh
+        {
+            $current_image = DB::table('staff')->where('id',$request->staff_id_input)->value('image');
+            $image_update = $current_image;
+        }    
+        
+
+        DB::table('staff')->where('id',$request->staff_id_input)->update([
+            'full_name' => $request->staff_name_input,
+            'image' => $image_update,
+            'phone' => $request->phone_input,
+            'citizen_id' => $request->citizen_id_input,
+            'date_of_birth' => $request->staff_birth_input
+        ]);
+        return redirect(route('foradmin.staff'));
     }
 }
