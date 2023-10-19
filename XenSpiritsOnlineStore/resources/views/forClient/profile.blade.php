db427cd234805cd9ac83c9bc98e1aaa58e95d9e1
thinhvuthecreator <thinhvuh@gmail.com>
2023-10-20 01:01:08 +0700

    dang lam giao dien profile khach hang

---
+++ XenSpiritsOnlineStore/resources/views/forClient/profile.blade.php
@@ -0,0 +1,8 @@
+<head>
+      <title>Xen. Spirits®</title>
+      <link rel="icon" href="{{asset('Resource/XenTitleIcon.png')}}" type="image/icon type">
+</head>
+<body>
+    @include('Reusable/on-top-page')
+    @include('Partial/customer/sidebar')
+</body>
