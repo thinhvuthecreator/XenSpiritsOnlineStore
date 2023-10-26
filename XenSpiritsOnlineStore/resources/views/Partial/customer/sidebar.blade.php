30dd07b43037aa6eee4fe7d210150c307b0a8a19
thinhvuthecreator <thinhvuh@gmail.com>
2023-10-26 12:30:09 +0700

    them giao dien customer dua tren giao dien admin template, chinh sua header va sidebar

--- XenSpiritsOnlineStore/resources/views/Partial/customer/sidebar.blade.php
+++ XenSpiritsOnlineStore/resources/views/Partial/customer/sidebar.blade.php
@@ -1,31 +1,77 @@
 <head>
-    <link rel="stylesheet" href="{{asset('CSS/customer-profile-style.css')}}">
+<link rel="stylesheet" href="{{asset('CSS/customer.sidebar.css')}}">
 </head>
 <body>
-<aside class="main-sidebar">
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+
     <!-- Sidebar -->
     <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{asset('AdminLTE/dist/img/twixim.png')}}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="{{ route('foradmin.profile') }}" class="d-block">TWIXIM_</a>
+        </div>
+        <div>
+          <br>
+          <a href="{{ route('logout') }}">Đăng xuất</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
       <!-- Sidebar Menu -->
       <nav class="mt-2">
-        <ul  data-widget="treeview" role="menu" data-accordion="false" class="sidebar-ul">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="#" class="nav-link">            
+            <a href="{{ route('foradmin.category') }}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
               <p>
-                Thông tin
+                Product Categories
               </p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="{{ route('foradmin.product') }}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
               <p>
-                Giỏ hàng
+                Products
               </p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="{{ route('foradmin.customer') }}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
               <p>
-                Đã mua
+                Customers
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ route('foradmin.staff') }}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Staffs
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ route('foradmin.role') }}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Roles
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ route('foradmin.account') }}" class="nav-link">
+              <i class="nav-icon fas fa-th"></i>
+              <p>
+                Accounts
               </p>
             </a>
           </li>
@@ -35,4 +81,4 @@
     </div>
     <!-- /.sidebar -->
   </aside>
-<body>
+</body>
--- XenSpiritsOnlineStore/resources/views/forClient/profile.blade.php
+++ XenSpiritsOnlineStore/resources/views/forClient/profile.blade.php
@@ -1,62 +1,24 @@
-<head>
-      <title>Xen. Spirits®</title>
-      <link rel="icon" href="{{asset('Resource/XenTitleIcon.png')}}" type="image/icon type">
-      <link rel="stylesheet" href="{{asset('CSS/customer-profile-style.css')}}">
-</head>
+@extends('Layout.Customer')
 
-<body>
-    @include('Reusable/on-top-page')
- <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script> -->
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<aside class="main-sidebar">
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul  data-widget="treeview" role="menu" data-accordion="false" class="sidebar-ul">
-          <li class="nav-item">
-            <a href="#" class="nav-link" id="info" onclick="click(event)">            
-              <p >
-                Thông tin
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link" id="cart" onclick="click(event)">
-              <p >
-                Giỏ hàng
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link" id="bill" onclick="click(event)">
-              <p>
-                Đã mua
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+@section('Title')
+<title>Xen. Spirits® | Khách hàng</title>
+@endsection
+
+
+@section('Content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    @include('./Partial/content_header', ['url1' => 'Account List' ,'url2' => 'Account', 'url3' => 'List'])
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
-    <!-- /.sidebar -->
-</aside>
-
-<script type="text/javascript">
-    $(document).ready(function(){
-        $("#info").click(function(e){
-            e.preventDefault();
-        $("#contentt").load("/profile-info");
-        });
-        $("#cart").click(function(e){
-            e.preventDefault();
-        $("#contentt").load("/products");
-        });
-    });
-</script>
-
-
-<div class="contentt" id="contentt">
-</div>
-
-</body>
+    <!-- /.content -->
+  </div>
+@endsection
